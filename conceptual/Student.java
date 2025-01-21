670e933a3072102c91cfdc1651d2a195745dc9c5
KRISH141006 <krishsondagar13@gmail.com>
2025-01-21 13:49:55 +0530

    added concept of parent child class

---
+++ conceptual/Student.java
@@ -0,0 +1,17 @@
+public class Student {
+    private String enroll;
+    private String name;
+    private int roll;
+    private String div;
+
+    public Student() {
+        this.enroll = "un";
+    }
+
+    public Student(String enroll, String name, int roll, String div) {
+        this.enroll = enroll;
+        this.name = name;
+        this.roll = roll;
+        this.div = div;
+    }
+}
---
+++ conceptual/TestStudent.java
@@ -0,0 +1,10 @@
+public class TestStudent {
+        public static void main(String[] args) {
+            Student studentOne = new Student();
+            Student studentTwo = new Student("123", "Arzoo", 47, "A");
+            System.out.println(studentOne);
+            System.out.println(studentTwo);
+        }    
+  
+}
+
